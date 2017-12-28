65bd36e63bee23550c5a4b8d6617ede54af041c1
Tijerina <tijerb@amazon.com>
2017-12-27 22:44:00 -0800

    this didnt upload originally. I panicked

---
+++ stuff.py
@@ -0,0 +1,46 @@
+string = "I\'m not sure how many characters this sentence has"
+name = "Brigittie"
+
+uppertest = "i will want to convert these to upper case"
+lowertest = "I WANT TO MAKE ALL THESE SMALL"
+counttest = "how that many that that can that I that count that in that this that sentence?"
+findtest = "I want to search for the position of the letter z"
+
+#print (len(string))
+#print (len(name))
+#print (uppertest.upper())
+#print(lowertest.lower())
+#print(counttest.count("that"))
+#print(findtest.find("z"))
+
+#print (*range(1,100))
+
+#for i in range(500,999):
+#    print (i)
+
+#for i in range(1,100):
+#    print (i)
+
+#/3
+#/3,5
+#/5
+
+for i in range(1,100):
+
+    if (i%3) == 0 and (i%5) == 0:
+        print ("boLth")
+    elif (i%3) == 0:
+        print ('divisible by 3 only')
+    elif (i%5) == 0:
+        print('divisible by 5 only')
+    else:
+        print(i)
+
+
+
+
+
+
+
+
+
